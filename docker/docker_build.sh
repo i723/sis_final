0ba561233b673f84fe5a656da0e0c0777e6f93a1
AnduSer37 <andy19970307@gmail.com>
2018-11-26 18:31:05 +0800

    push template

---
+++ docker/docker_build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+docker build -t sis_competition -f Dockerfile .
---
+++ docker/run_task.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+source ~/sis_mini_competition_2018/catkin_ws/devel/setup.sh
+
+roslaunch sis_arm_planning master_task.launch
